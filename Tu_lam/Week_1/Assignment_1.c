9d318047ec603eae4e96070047f4488b179a2b2c
Reborn92 <ducanhtc@gmail.com>
2020-06-12 15:13:52 +0700

    Revert "Up lần 1 _ test github"
    This reverts commit bb341f661f4d74093145d72a2357887ab95cedd4.

--- Tu_lam/Week_1/Assignment_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-// Bai tap ghi het ra cac ma ASCCII
-int main(){
-    int i;
-    printf("Bang ma ASCCII la: \n");
-    for (i = 0; i < 128; i++){
-        printf("%5d%5c\n" ,i ,i); // %5d va %5c khac %d%c o cho 5 la khoang cach truoc khi in
-    }
-}
